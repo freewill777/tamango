4fd2dc937496c1b80182f6795837b0422a1ceff1
Lucian Buzea <lucianb@playtika.com>
2023-04-15 19:42:24 +0300

    inserting mediafiles in collection upon upload

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ app.post('/register', asyncHandler(async (req, res) => {
             password,
             email: email.toLowerCase(),
             stats: {
-                posts: 0,
-                videos: 0,
-                followers: 0,
-                following: 0,
+                posts: [],
+                videos: [],
+                followers: [],
+                following: [],
                 description1: '',
                 description2: '',
                 occupation: '',
@@ -96,30 +96,47 @@ app.get('/posts', asyncHandler(async (req, res) => {
     }
 }));
 
-function mediaTypeMap(mimetype) {
-    if (mimetype.includes('image')) {
-        return 'images'
-    }
-}
+// function mediaTypeMap(mimetype) {
+//     if (mimetype.includes('image')) {
+//         return 'images'
+//     }
+//     if (mimetype.includes('video')) {
+//         return 'videos'
+//     }
+//     if (mimetype.includes('story')) {
+//         return 'stories'
+//     }
+// }
 
 const multer = require("multer");
 const {SERVER_PORT} = require("./settings");
 const {settings} = require("express/lib/application");
+let insertedid = ''
 const upload = multer({
     storage: multer.diskStorage({
-        destination: (req, file, cb) => {
+        destination: async (req, file, cb) => {
             const userId = req.headers.userid
-            const mimetype = file.mimetype
-            const dir = `uploads/${userId}/${mediaTypeMap(mimetype)}`;
+            const mediaType = req.headers.mediatype
+            const dir = `uploads/${userId}/${mediaType}`;
 
             if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir, { recursive: true });
             }
             cb(null, dir);
+
+            const collection = mongoose.connection.db.collection('mediafiles')
+            const { insertedId } = await collection.insertOne({
+                userId,
+                mediaType,
+                likes: [],
+                comments: []
+            })
+            insertedid = insertedId
+
         },
         filename: (req, file, cb) => {
             const ext = MIME_TYPE_MAP[file.mimetype];
-            const name = String(Date.now()) + '.' + ext;
+            const name = String(insertedid) + '.' + ext;
             if (!!ext) {
                 cb(null, name);
             } else {
@@ -138,8 +155,7 @@ const MIME_TYPE_MAP = {
     'video/quicktime': 'quicktime'
 };
 
-app.post("/upload_files", upload.array("files"), function (req, res) {
-    userId = req.body.userId
+app.post("/upload_files", upload.array("files"), async function (req, res) {
     res.json({ message: "Successfully uploaded files" });
 });
 
