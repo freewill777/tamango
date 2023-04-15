eefd6b2403822b648cc981fd97dfb481b1eca12b
Lucian Buzea <lucianb@playtika.com>
2023-04-15 21:17:40 +0300

    working LIKE endpoint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cors = require('cors')
 const asyncHandler = require('express-async-handler')
 const app = express();
 const ObjectId = require('mongoose').Types.ObjectId;
-const { MONGO_URL } = require("./settings");
+const {MONGO_URL} = require("./settings");
 var fs = require('fs');
 
 let router = express.Router();
@@ -17,18 +17,18 @@ mongoose.connect(MONGO_URL)
 
 app.get('/login', asyncHandler(async (req, res) => {
     try {
-        var { email, password } = req.query
+        var {email, password} = req.query
         const collection = mongoose.connection.db.collection('users')
-        const cursor = collection.find({ email: email.toLowerCase() })
+        const cursor = collection.find({email: email.toLowerCase()})
         const usersFound = await cursor.toArray()
         if (usersFound.length === 0) {
             res.status(500).send('User does not exist');
             return
         }
         const user = usersFound[0]
-        const { _id } = user
+        const {_id} = user
         if (password === user.password) {
-            res.json({ id: _id });
+            res.json({id: _id});
         } else {
             res.status(401).json({});
         }
@@ -40,15 +40,15 @@ app.get('/login', asyncHandler(async (req, res) => {
 
 app.post('/register', asyncHandler(async (req, res) => {
     try {
-        var { name, password, email } = req.query
+        var {name, password, email} = req.query
         const collection = mongoose.connection.db.collection('users')
-        const cursor = collection.find({ name: name.toLowerCase() })
+        const cursor = collection.find({name: name.toLowerCase()})
         const usersFound = await cursor.toArray()
         if (usersFound.length > 0) {
             res.status(500).send('User already exists');
             return
         }
-        const { insertedId: id } = await collection.insertOne({
+        const {insertedId: id} = await collection.insertOne({
             name: name.toLowerCase(),
             password,
             email: email.toLowerCase(),
@@ -63,7 +63,7 @@ app.post('/register', asyncHandler(async (req, res) => {
                 avatar: ''
             }
         })
-        res.json({ id });
+        res.json({id});
     } catch (err) {
         console.error(`Error retrieving data: ${err.message}`);
         res.status(500).send('Internal server error');
@@ -72,9 +72,9 @@ app.post('/register', asyncHandler(async (req, res) => {
 
 app.get('/user', asyncHandler(async (req, res) => {
     try {
-        const { id } = req.query
+        const {id} = req.query
         const collection = mongoose.connection.db.collection('users')
-        const cursor = collection.find({ _id: new ObjectId(id) })
+        const cursor = collection.find({_id: new ObjectId(id)})
         const usersFound = await cursor.toArray()
         res.json(usersFound[0]);
     } catch (err) {
@@ -83,44 +83,87 @@ app.get('/user', asyncHandler(async (req, res) => {
     }
 }));
 
-app.get('/posts', asyncHandler(async (req, res) => {
+app.get('/mediafiles', asyncHandler(async (req, res) => {
     try {
-        const { id } = req.query
-        const collection = mongoose.connection.db.collection('posts')
-        const cursor = collection.find({})
+        const {userId} = req.query
+        const collection = mongoose.connection.db.collection('mediafiles')
+        const cursor = collection.find({userId})
         const posts = await cursor.toArray()
-        res.json({ id, posts });
+        res.json({userId, posts});
     } catch (err) {
         console.error(`Error retrieving data: ${err.message}`);
         res.status(500).send('Internal server error');
     }
 }));
 
-// function mediaTypeMap(mimetype) {
-//     if (mimetype.includes('image')) {
-//         return 'images'
-//     }
-//     if (mimetype.includes('video')) {
-//         return 'videos'
-//     }
-//     if (mimetype.includes('story')) {
-//         return 'stories'
-//     }
-// }
+app.post('/like', asyncHandler(async (req, res) => {
+    try {
+        const {userId, mediafileId} = req.query
+        const collection = mongoose.connection.db.collection('mediafiles')
+        const filter = {_id: new ObjectId(mediafileId)}
+        const cursor = collection.find(filter)
+        const mediafile = await cursor.toArray()
+        const {likes} = mediafile[0]
+        const isLiked = likes.includes(userId)
+
+        console.log(likes)
+        const likeMediafile = {
+            $push: {
+                likes: userId
+            }
+        }
+        const unlikeMediafile = {
+            $pull: {
+                likes: userId
+            }
+        }
+        if (isLiked) {
+            collection.updateOne(filter, unlikeMediafile)
+            res.json({userId,mediafileId,message:"Unliked!"});
+        } else {
+            collection.updateOne(filter, likeMediafile)
+            res.json({userId,mediafileId,message:"Liked!"});
+        }
+    } catch (err) {
+        console.error(`Error retrieving data: ${err.message}`);
+        res.status(500).send('Internal server error');
+    }
+}));
+
+app.post('/unlike', asyncHandler(async (req, res) => {
+    try {
+        const {userId, mediafileId} = req.query
+        const collection = mongoose.connection.db.collection('mediafiles')
+        const filter = {_id: new ObjectId(mediafileId)}
+        const updateMediafile = {
+            $set: {
+                likes: likes.filter(like => like !== userId)
+            }
+        }
+        const cursor = collection.updateOne(filter, updateMediafile)
+        const mediafile = await cursor.toArray()
+        res.json({mediafile});
+    } catch (err) {
+        console.error(`Error retrieving data: ${err.message}`);
+        res.status(500).send('Internal server error');
+    }
+}));
 
 const multer = require("multer");
 const {SERVER_PORT} = require("./settings");
 const {settings} = require("express/lib/application");
 let insertedid = ''
+let dir = ''
+let ext = ''
 const upload = multer({
     storage: multer.diskStorage({
         destination: async (req, file, cb) => {
             const userId = req.headers.userid
             const mediaType = req.headers.mediatype
-            const dir = `uploads/${userId}/${mediaType}`;
+            dir = `uploads/${userId}/${mediaType}`;
 
             if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir, { recursive: true });
+                fs.mkdirSync(dir, {recursive: true});
             }
             cb(null, dir);
 
@@ -129,13 +172,14 @@ const upload = multer({
                 userId,
                 mediaType,
                 likes: [],
-                comments: []
+                comments: [],
+                path: dir + insertedId + '.' + ext
             })
             insertedid = insertedId
 
         },
         filename: (req, file, cb) => {
-            const ext = MIME_TYPE_MAP[file.mimetype];
+            ext = MIME_TYPE_MAP[file.mimetype];
             const name = String(insertedid) + '.' + ext;
             if (!!ext) {
                 cb(null, name);
@@ -156,7 +200,7 @@ const MIME_TYPE_MAP = {
 };
 
 app.post("/upload_files", upload.array("files"), async function (req, res) {
-    res.json({ message: "Successfully uploaded files" });
+    res.json({message: "Successfully uploaded files"});
 });
 
 app.listen(SERVER_PORT, () => {
